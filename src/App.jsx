0f50fdfb677656b14be4312da131b03c0061a289
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-03 17:47:53 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Image from "./components/Image";
 function App() {
   return (
     <>
-      <Btn btnTxt="ciao"></Btn>
+      <Btn btnTxt="ciao" />
       <Image
         imgUrl="https://m.media-amazon.com/images/S/pv-target-images/dbf6812f59e5080cf420f1056bfceb66f7d6a43a8df19ace503ea70596afc0ff.jpg"
         imgWidth="300px"
-      ></Image>
+      />
     </>
   );
 }
